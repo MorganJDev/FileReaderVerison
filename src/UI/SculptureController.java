e82d5b35aec1a48028ca7417c8bff41493953f83
MorganJDev <morganjones708@gmail.com>
2017-12-05 17:24:48 +0000

    Added Sculpture

---
+++ src/UI/SculptureController.java
@@ -0,0 +1,19 @@
+package UI;
+
+import javafx.fxml.FXML;
+
+public class SculptureController
+{
+    @FXML
+    private void handleOk()
+    {
+
+    }
+
+    @FXML
+    private void handleCancel()
+    {
+        Main.popup.close();
+    }
+
+}
