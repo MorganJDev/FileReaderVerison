cf74990d3390cddd5ab911fb9fd605251b2430d4
gogo2497 <33661806+gogo2497@users.noreply.github.com>
2017-12-05 20:38:59 +0000

    Add files via upload

--- src/UI/CanvasController.java
+++ src/UI/CanvasController.java
@@ -1,6 +1,15 @@
 package UI;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
+import javafx.stage.Stage;
+import java.awt.Canvas;
+import javafx.scene.paint.Color;
+
+import java.awt.*;
 
 /**
  * @author Georgi  Georgiev
@@ -8,9 +17,55 @@ import javafx.fxml.FXML;
 
 public class CanvasController
 {
-    @FXML
+    ColorPicker cp;
+    @FXML javafx.scene.canvas.Canvas canvasPane;
     private void handleCancel()
     {
         Main.popup.close();
+    }
+
+    private void drawCircle(){
+
+    }
+
+    @FXML
+    private void handleLineClick()
+    {
+        drawLine();
+    }
+
+    @FXML
+    private void handleDraw()
+    {
+        //drawLine();
+    }
+
+    private void drawLine(){
+        cp = new ColorPicker();
+
+        GraphicsContext gc = canvasPane.getGraphicsContext2D();
+        gc.setStroke(Color.BLACK);
+        gc.setLineWidth(1);
+
+        canvasPane.setOnMousePressed(e -> {
+            gc.beginPath();
+            gc.lineTo(e.getSceneX() - 185, e.getSceneY() - 70);
+            gc.stroke();
+        });
+
+        canvasPane.setOnMouseDragged(e -> {
+            gc.lineTo(e.getSceneX() - 185, e.getSceneY() - 70);
+            gc.stroke();
+        });
+
+
+        cp.setValue(Color.BLACK);
+        cp.setOnAction(e -> {
+            gc.setStroke(cp.getValue());
+        });
+    }
+
+    private void drawReactangle(){
+
     }
 }
