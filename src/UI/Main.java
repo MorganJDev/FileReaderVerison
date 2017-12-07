71d0b470161de00da0185e418ffea2b65205f14c
Georgi <gogo24971@abv.bg>
2017-12-07 16:38:13 +0000

    Canvas stuff

--- src/UI/Main.java
+++ src/UI/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application
 	@Override
 	public void start(Stage primaryStage) throws Exception
     {
-        SetupApplication();
+        //SetupApplication();
 
         Stage newWindow = primaryStage;
 
