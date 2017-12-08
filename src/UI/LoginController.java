bc55f71b304f17095e8614faf1a60d20ea6bea76
Morgan Jones <morganjones708@gmail.com>
2017-12-08 17:18:52 +0000

    Fixed remaining bids. Compare string using .equals()

--- src/UI/LoginController.java
+++ src/UI/LoginController.java
@@ -53,6 +53,10 @@ public class LoginController
 
             Main.window.setOnCloseRequest(e -> Main.closeApplication());
         }
+        else
+        {
+            usernameBox.setText("");
+        }
     }
 
     /**
