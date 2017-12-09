1d237e85f71f8902e7853a22a00e857d9dc85f01
Morgan Jones <morganjones708@gmail.com>
2017-12-09 19:28:08 +0000

    Removing admin

--- src/UI/CanvasController.java
+++ src/UI/CanvasController.java
@@ -1,25 +1,33 @@
 
 package UI;
 
+import Classes.Custom;
 import Classes.User;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 //import javafx.scene.control.ColorPicker;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.image.WritableImage;
 import javafx.scene.shape.Circle;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import java.awt.Canvas;
 import javafx.scene.paint.Color;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.RenderedImage;
 import java.awt.image.renderable.RenderableImage;
 import java.awt.image.renderable.RenderableImageOp;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * @author Georgi  Georgiev
@@ -38,18 +46,54 @@ public class CanvasController
     }
 
     @FXML
-    private void handleSave()
+    private void handleSave() throws Exception
     {
-        WritableImage newWI = new WritableImage((int) canvasPane.getWidth() + 20,
-                (int) canvasPane.getHeight() + 20);
+        FileChooser fileC = new FileChooser();
 
-        canvasPane.snapshot(null,newWI);
-        RenderableImage newRI = (RenderableImage) SwingFXUtils.fromFXImage(newWI,null);
+        // Extension
+        FileChooser.ExtensionFilter extension =
+                new FileChooser.ExtensionFilter("png files (*png)", "*png");
+        fileC.getExtensionFilters().add(extension);
 
-        //ImageIO.write(newRI,"png",filePath);
+        // Set file path
+        File file = new File("AllImages/CustomImages/" + Main.admin.getCurrentUser().getUsername() + ".png");
 
+        if(file != null)
+        {
+            try
+            {
+                WritableImage newWI = new WritableImage((int) canvasPane.getWidth(), (int) canvasPane.getHeight());
+                canvasPane.snapshot(null, newWI);
+                RenderedImage newRI = SwingFXUtils.fromFXImage(newWI, null);
+                ImageIO.write(newRI, "png", file);
+
+                findUser().setProfileImage("/CustomImages/" + Main.admin.getCurrentUser().getUsername() + ".png");
+
+                reloadMain();
+            }
+            catch (IOException ex)
+            {
+                System.out.println("Error saving");
+            }
+        }
 
        // findUser().setProfileImage
+    }
+
+    private void reloadMain() throws Exception
+    {
+        // Sets a Main static variable that's referenced on MainMenu initialisation
+        Main.reloadedOn = "Profile";
+
+        Parent current = FXMLLoader.load(getClass().getResource("MainMenu.fxml"));
+
+        Main.window.close();
+        Main.window.setScene(new Scene(current));
+        Main.window.show();
+
+        Main.window.setOnCloseRequest(e -> Main.closeApplication());
+
+        Main.popup.close();
     }
 
     private User findUser() {
